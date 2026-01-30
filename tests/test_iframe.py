c6dd8d8b41a9ae4d62295e4ce55c16573a1513c6
junior.matiello <junior.matiello@senior.com.br>
2026-01-30 15:00:41 -0300

    #AULA39 - iframe

---
+++ tests/test_iframe.py
@@ -0,0 +1,7 @@
+
+def test_iframe(page):
+    page.goto("https://www.w3schools.com/html/tryit.asp?filename=tryhtml_iframe_height_width")
+    iframe = page.frame_locator("#iframeResult")
+    inner_iframe = iframe.frame_locator('[src="demo_iframe.htm"]')
+    texto = inner_iframe.locator('h1').inner_text()
+    print(texto)
