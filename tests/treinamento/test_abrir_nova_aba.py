d4be42b4ec3363ba4adf9c233db4e40e84ef79dd
junior.matiello <junior.matiello@senior.com.br>
2026-02-02 09:13:15 -0300

    #AULA43 - Trabalhando com Novas Abas

---
+++ tests/treinamento/test_abrir_nova_aba.py
@@ -0,0 +1,13 @@
+
+
+def test_abrir_nova_aba(page):
+    # Navegar para a página inicial
+    page.goto("https://demoqa.com/browser-windows")
+    page.pause()
+
+    with page.expect_popup() as popup_info:
+        page.locator("#tabButton").click()
+
+    nova_aba = popup_info.value
+    nova_aba.wait_for_load_state()
+    page.pause()
